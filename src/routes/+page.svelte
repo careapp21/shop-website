8fa63693fc047326b2d8adc14b5b54db705ec7de
littlechicks <childcareapp21@gmail.com>
2025-10-02 17:39:07 +0100

    first draft

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import poster from '$lib/assets/poster.png';
+</script>
+<div class="flex flex-col justify-center items-center text-center">
+    <img src={poster} alt="Poster">
+</div>
